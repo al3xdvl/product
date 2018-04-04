2f5a342613d6fa380755df6a019b9e219e2eb581
Alex Deval <al3x.dvl@gmail.com>
2018-04-04 18:47:34 +0200

    user amelioration

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,14 +2,25 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+  # content title: proc{ I18n.t("active_admin.dashboard") } do
+  #   div class: "blank_slate_container", id: "dashboard_default_message" do
+  #     span class: "blank_slate" do
+  #       span I18n.t("active_admin.dashboard_welcome.welcome")
+  #       small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #     end
+  #   end
 
+  content do
+
+  panel "Recent Products" do
+    table_for Prod.order("created_at desc").limit(5) do
+      column :name
+      column :user
+      column :created_at
+    end
+    strong { link_to "View All Products", admin_products_path }
+  end
+  end
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
@@ -29,5 +40,5 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
-  end # content
+# content
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prods#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :update, :show]
   resources :prices
   resources :prods do
     resources :prices
